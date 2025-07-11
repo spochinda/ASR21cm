a78c7163df1fc0a264fe1867cde205798b63e689
Simon Pochinda <sp2053@cam.ac.uk>
2025-07-11 12:23:48 +0100

    Update redshift_scaling.py to parameterize Npix and IC, ensure device compatibility, and save output plots in a dedicated directory

--- analysis/redshift_scaling.py
+++ analysis/redshift_scaling.py
@@ -19,18 +19,23 @@ if __name__ == '__main__':
     current_dir = os.path.dirname(os.path.abspath(__file__))
     root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-    T21_hr = ['T21_cube_z14__Npix256_IC20.mat', 'T21_cube_z15__Npix256_IC20.mat', 'T21_cube_z16__Npix256_IC20.mat', 'T21_cube_z17__Npix256_IC20.mat', 'T21_cube_z18__Npix256_IC20.mat', 'T21_cube_z19__Npix256_IC20.mat', 'T21_cube_z20__Npix256_IC20.mat', 'T21_cube_z21__Npix256_IC20.mat']
+    Npix = 512
+    IC = 0
+    z = list(range(14, 22))
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    T21_hr = [f'T21_cube_z{zi}__Npix{Npix}_IC{IC}.mat' for zi in z]
     T21_hr = [loadmat(os.path.join(root_dir, 'datasets/varying_IC/T21_cubes', file))['Tlin'] for file in T21_hr]
     T21_hr = [torch.from_numpy(t).unsqueeze(0).unsqueeze(0).to(torch.float32) for t in T21_hr]
-    T21_hr = torch.concat(T21_hr, dim=0)
+    T21_hr = torch.concat(T21_hr, dim=0).to(device)
 
-    delta = loadmat(os.path.join(root_dir, 'datasets/varying_IC/IC_cubes/delta_Npix256_IC20.mat'))['delta']
-    delta = torch.from_numpy(delta).unsqueeze(0).unsqueeze(0).to(torch.float32)  # Convert to BCHWD format
+    delta = loadmat(os.path.join(root_dir, f'datasets/varying_IC/IC_cubes/delta_Npix{Npix}_IC{IC}.mat'))['delta']
+    delta = torch.from_numpy(delta).unsqueeze(0).unsqueeze(0).to(torch.float32).to(device)  # Convert to BCHWD format
     delta, _, _ = utils.normalize(delta, mode='standard')
-    vbv = loadmat(os.path.join(root_dir, 'datasets/varying_IC/IC_cubes/vbv_Npix256_IC20.mat'))['vbv']
-    vbv = torch.from_numpy(vbv).unsqueeze(0).unsqueeze(0).to(torch.float32)  # Convert to BCHWD format
+    vbv = loadmat(os.path.join(root_dir, f'datasets/varying_IC/IC_cubes/vbv_Npix{Npix}_IC{IC}.mat'))['vbv']
+    vbv = torch.from_numpy(vbv).unsqueeze(0).unsqueeze(0).to(torch.float32).to(device)  # Convert to BCHWD format
     vbv, _, _ = utils.normalize(vbv, mode='standard')
-    z = torch.tensor([14., 15., 16., 17., 18., 19., 20., 21.])  # Example z vector
+    z = torch.tensor(z, dtype=torch.float32).to(device)  # Convert z to a tensor
 
     print(f'T21_hr shape: {T21_hr.shape}', flush=True)
     print(f'delta shape: {delta.shape}', flush=True)
@@ -41,7 +46,7 @@ if __name__ == '__main__':
         opt = yaml.load(f, Loader=ordered_yaml()[0])
     opt['rank'], opt['world_size'] = get_dist_info()
     opt['is_train'] = False
-    opt['num_gpu'] = 0
+    opt['num_gpu'] = torch.cuda.device_count()
     results_root = os.path.join(root_dir, 'results', opt['name'])
     opt['path']['results_root'] = results_root
     opt['path']['log'] = results_root
@@ -49,35 +54,37 @@ if __name__ == '__main__':
 
     net_g = build_network(opt['network_g'])
     load_path = os.path.join(root_dir, 'good_experiments/ASR21cm_GAN_z_conditional_3/models/net_g_2000.pth')
-    load_net = torch.load(load_path, map_location=lambda storage, loc: storage)
+    load_net = torch.load(load_path, map_location=device)
     net_g.load_state_dict(load_net['params_ema'], strict=True)
+    net_g = net_g.to(device)
 
     b, c, h, w, d = T21_hr.shape
     xyz_hr = make_coord([h, h, h], ranges=None, flatten=False)
     xyz_hr = xyz_hr.view(1, -1, 3)
-    xyz_hr = xyz_hr.repeat(1, 1, 1)
+    xyz_hr = xyz_hr.repeat(1, 1, 1).to(device)
 
     T21_sr = []
     T21_lr = []
     net_g.eval()
     for i in tqdm(range(len(T21_hr)), desc='Scaling...', total=len(T21_hr)):
-        input = torch.nn.functional.interpolate(T21_hr[i:i + 1], size=64, mode='trilinear')
+        input = torch.nn.functional.interpolate(T21_hr[i:i + 1], size=Npix//4, mode='trilinear')
         input_mean = torch.mean(input, dim=(1, 2, 3, 4), keepdim=True)
         input_std = torch.std(input, dim=(1, 2, 3, 4), keepdim=True)
         input, _, _ = utils.normalize(input, mode='standard')
 
         with torch.no_grad():
-            # output, _ = net_g(img_lr=input, xyz_hr=xyz_hr, delta=delta, vbv=vbv, z=z[i:i+1])
-            # T21_sr.append(output * input_std + input_mean)
+            #output, _ = net_g(img_lr=input, xyz_hr=xyz_hr, delta=delta, vbv=vbv, z=z[i:i+1], chunks=40)
+            output = torch.rand_like(delta)  # Placeholder for the actual output
+            T21_sr.append(output * input_std + input_mean)
             input = input * input_std + input_mean  # denormalize input
-            input = torch.nn.functional.interpolate(input, size=256, mode='trilinear')  # upsample input to match output size
+            input = torch.nn.functional.interpolate(input, size=Npix, mode='trilinear')  # upsample input to match output size
             T21_lr.append(input)
-    torch.cuda.empty_cache()
+        torch.cuda.empty_cache()
 
     T21_lr = torch.concat(T21_lr, dim=0)
-    # T21_sr = torch.concat(T21_sr, dim=0)
-    # torch.save(T21_sr, os.path.join(current_dir, 'T21_sr_z.pt'))
-    T21_sr = torch.load(os.path.join(current_dir, 'T21_sr_z.pt'))
+    T21_sr = torch.concat(T21_sr, dim=0)
+    #torch.save(T21_sr, os.path.join(current_dir, 'files', 'T21_sr_z.pt'))
+    T21_sr = torch.load(os.path.join(current_dir, 'files', 'T21_sr_z.pt'))
 
     print(f'T21_sr shape: {T21_sr.shape}', flush=True)
     print(f'T21_lr shape: {T21_lr.shape}', flush=True)
@@ -86,9 +93,9 @@ if __name__ == '__main__':
     # matplotlib settings
     rasterized = False
     plt.rcParams.update({
-        'text.usetex': True,
-        'font.family': 'serif',
-        'font.serif': 'cm',
+        #'text.usetex': True,
+        #'font.family': 'serif',
+        #'font.serif': 'cm',
         'font.size': 22,
     })
 
@@ -116,33 +123,35 @@ if __name__ == '__main__':
             if row == 0 or row == 2:
                 axes[row, col].tick_params(labelbottom=False)  # Hide x-ticks for the first dsq row
 
-    slice_idx = 128
-
-    k_hr, dsq_hr = calculate_power_spectrum(data_x=T21_hr, Lpix=3, kbins=100, dsq=True, method='torch', device='cpu')
+    k_hr, dsq_hr = calculate_power_spectrum(data_x=T21_hr, Lpix=3, kbins=100, dsq=True, method='torch', device=device)
+    k_hr = k_hr.cpu()
+    dsq_hr = dsq_hr.cpu()
 
     progress_bar = tqdm(total=8, desc='Plotting...', position=0, leave=True)
     for row in range(2):
         for col in range(4):
             idx = np.ravel_multi_index((row, col), (2, 4))
-            k_sr, dsq_sr = calculate_power_spectrum(data_x=T21_sr[idx:idx + 1], Lpix=3, kbins=100, dsq=True, method='torch', device='cpu')
-            k_lr, dsq_lr = calculate_power_spectrum(data_x=T21_lr[idx:idx + 1], Lpix=3, kbins=100, dsq=True, method='torch', device='cpu')
+            k_sr, dsq_sr = calculate_power_spectrum(data_x=T21_sr[idx:idx + 1], Lpix=3, kbins=100, dsq=True, method='torch', device=device)
+            k_sr, dsq_sr = k_sr.cpu(), dsq_sr.cpu()
+            k_lr, dsq_lr = calculate_power_spectrum(data_x=T21_lr[idx:idx + 1], Lpix=3, kbins=100, dsq=True, method='torch', device=device)
+            k_lr, dsq_lr = k_lr.cpu(), dsq_lr.cpu()
 
-            xmin = min(T21_sr[idx].min(), T21_hr[idx].min())
-            xmax = max(T21_sr[idx].max(), T21_hr[idx].max())
+            xmin = min(T21_sr[idx].min().cpu().numpy(), T21_hr[idx].min().cpu().numpy(), T21_lr[idx].min().cpu().numpy())
+            xmax = max(T21_sr[idx].max().cpu().numpy(), T21_hr[idx].max().cpu().numpy(), T21_lr[idx].max().cpu().numpy())
             bins = np.linspace(xmin, xmax, 100)
             axes[row, col].hist(T21_hr[idx].cpu().numpy().flatten(), bins=bins, alpha=0.5, label='HR', histtype='step', linewidth=4, density=True, zorder=2, rasterized=rasterized)
             axes[row, col].hist(T21_sr[idx].cpu().numpy().flatten(), bins=bins, alpha=0.5, label='SR', histtype='step', linewidth=4, density=True, zorder=3, rasterized=rasterized)
             axes[row, col].hist(T21_lr[idx].cpu().numpy().flatten(), bins=bins, alpha=0.5, label='LR', histtype='step', linewidth=4, density=True, zorder=1, rasterized=rasterized)
             rmse_sr = torch.sqrt(torch.mean((T21_sr[idx] - T21_hr[idx])**2))
             rmse_lr = torch.sqrt(torch.mean((T21_lr[idx] - T21_hr[idx])**2))
-            axes[row, col].text(0.05, 0.95, rf'$z={z[idx]:.0f}$' + '\n' + rf'$\mathrm{{RMSE}}_{{SR}} = {rmse_sr.item():.2f}\ \mathrm{{mK}}$' + '\n' + rf'$\mathrm{{RMSE}}_{{LR}} = {rmse_lr.item():.2f}\ \mathrm{{mK}}$', transform=axes[row, col].transAxes, fontsize=plt.rcParams['font.size'] - 2, ha='left', va='top')
+            axes[row, col].text(0.05, 0.95, rf'$z={z[idx]:.0f}$' + '\n' + rf'$\mathrm{{RMSE}}{{T_{21}}}_{{SR}} = {rmse_sr.item():.2f}\ \mathrm{{mK}}$' + '\n' + rf'$\mathrm{{RMSE}}^{{T_{{21}}}}_{{LRI}} = {rmse_lr.item():.2f}\ \mathrm{{mK}}$', transform=axes[row, col].transAxes, fontsize=plt.rcParams['font.size'] - 2, ha='left', va='top')
 
             axes[row + 2, col].loglog(k_hr, dsq_hr[idx, 0], label='$T_{{21}}$ HR', ls='solid', lw=2, rasterized=rasterized, zorder=2)
             axes[row + 2, col].loglog(k_sr, dsq_sr[0, 0], label='$T_{{21}}$ SR', ls='solid', lw=2, rasterized=rasterized, zorder=3)
             axes[row + 2, col].loglog(k_lr, dsq_lr[0, 0], label='$T_{{21}}$ LR (Interpolated)', ls='solid', lw=2, rasterized=rasterized, zorder=1)
             rmse_dsq_sr = torch.sqrt(torch.nanmean((dsq_sr - dsq_hr)**2))
             rmse_dsq_lr = torch.sqrt(torch.nanmean((dsq_lr - dsq_hr)**2))
-            axes[row + 2, col].text(0.05, 0.95, rf's={256 / 64:.2f}, z={z[0].item():.0f}' + '\n' + rf'$\mathrm{{RMSE}}_{{SR}}^{{\Delta^2}} = {rmse_dsq_sr.item():.2f}\ \mathrm{{mK}}^2$' + '\n' + rf'$\mathrm{{RMSE}}_{{LR}}^{{\Delta^2}} = {rmse_dsq_lr.item():.2f}\ \mathrm{{mK}}^2$', transform=axes[row + 2, col].transAxes, fontsize=plt.rcParams['font.size'] - 2, ha='left', va='top')
+            axes[row + 2, col].text(0.05, 0.95, rf'$s=4$, $z={z[idx].item():.0f}$' + '\n' + rf'$\mathrm{{RMSE}}^{{\Delta^2}}_{{SR}} = {rmse_dsq_sr.item():.2f}\ \mathrm{{mK}}^2$' + '\n' + rf'$\mathrm{{RMSE}}^{{\Delta^2}}_{{LRI}} = {rmse_dsq_lr.item():.2f}\ \mathrm{{mK}}^2$', transform=axes[row + 2, col].transAxes, fontsize=plt.rcParams['font.size'] - 2, ha='left', va='top')
 
             if row == 0:
                 axes[row + 1, col].set_xlabel('$T_{21}$ [$\\mathrm{mK}$]')
@@ -170,8 +179,9 @@ if __name__ == '__main__':
     # axes[0, 0].legend(fontsize=plt.rcParams['font.size'] - 2, frameon=False, loc='center left')
 
     axes[1, 0].set_xlim(-220, 10)
-    axes[3, 0].set_ylim(1e0, 1e3)
+    axes[1, 0].set_ylim(0, 0.095)
+    axes[3, 0].set_ylim(2e-1, 1e3)
 
-    save_img_path = os.path.join(current_dir, 'redshift_scaling.pdf')
+    save_img_path = os.path.join(current_dir, 'plots',f'redshift_scaling_Npix{Npix}.pdf')
     plt.savefig(save_img_path, bbox_inches='tight')
     plt.close(fig)
